3d62e3b397489e77f12ccb40fdb09a58ae8f7078
neomill <rneomillmarc@gmail.com>
2026-06-13 20:54:39 +0800

    feat: connect to postgress database

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,36 +1,43 @@
 import { Router, type Request, type Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { pool } from "../config/db.ts";
 
 const router = Router();
-const JWT_SECRET = process.env["JWT_SECRET"] || "fallback_secret";
+const JWT_SECRET = process.env["JWT_SECRET"];
 
-// @todo: Implement user registration and login routes here
-interface User {
-  id: string;
-  email: string;
-  passwordHash: string;
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
 }
-const users: User[] = []; // In-memory user store (replace with DB in production)
 
 router.post("/register", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password)
-      return res.status(400).json({
-        message: "missing fields",
-      });
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "missing fields" });
+    }
+
+    // check if user exists
+    const existingUser = await pool.query(
+      "SELECT id FROM users WHERE email = $1",
+      [email],
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(409).json({ message: "User already exists" });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    users.push({
-      id: crypto.randomUUID(),
-      email,
-      passwordHash: hashedPassword,
-    });
+    const result = await pool.query(
+      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email",
+      [email, hashedPassword],
+    );
 
     return res.status(201).json({
       message: "User registered successfully",
+      user: result.rows[0],
     });
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
@@ -40,12 +47,22 @@ router.post("/register", async (req: Request, res: Response) => {
 router.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    const user = users.find((u) => u.email === email);
 
-    if (!user || !(await bcrypt.compare(password, user.passwordHash))) {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    const user = result.rows[0];
+
+    if (!user) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
+    const isValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
     const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
       expiresIn: "1h",
     });
